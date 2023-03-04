a41672ee8bf8ed25b79de25db36361dbcb1f0107
fratk21 <67985345+fratk21@users.noreply.github.com>
2023-03-04 17:41:19 +0300

    Add files via upload

---
+++ ios/OneSignalNotificationExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationExtension
+//
+//  Created by Selective on 4.03.2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
